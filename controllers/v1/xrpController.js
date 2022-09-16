218da0f8d483dc2decfcb6faffe2eb96c7d5c852
dorbor <www.dorborrichards.com>
2022-09-16 02:12:41 -0700

    xrp api update

--- controllers/v1/xrpController.js
+++ controllers/v1/xrpController.js
@@ -30,8 +30,8 @@ exports.createOffer = async (req, res, next) => {
   
     // Get credentials from the Testnet Faucet -----------------------------------
     console.log("Requesting address ...")
-    const wallet = xrpl.Wallet.fromSeed(body.secret)
-    console.log(`Got address ${wallet.address}.`)
+    const wallet = body
+    console.log(`Got address ${wallet}.`)
     const we_want = {
       currency: "BTC",
       issuer: wallet.address,
@@ -135,21 +135,21 @@ exports.createOffer = async (req, res, next) => {
     }
   
     const prepared = await client.autofill(offer_1)
-    console.log("Prepared transaction:", JSON.stringify(prepared, null, 2))
-    const signed = wallet.sign(prepared)
-    console.log("Sending OfferCreate transaction...")
-    const result = await client.submitAndWait(signed.tx_blob)
-    if (result.result.meta.TransactionResult == "tesSUCCESS") {
-      console.log(`Transaction succeeded:
-            https://testnet.xrpl.org/transactions/${signed.hash}`)
-    } else {
-      throw `Error sending transaction: ${result}`
-    }
+    // console.log("Prepared transaction:", JSON.stringify(prepared, null, 2))
+    // const signed = wallet.sign(prepared)
+    // console.log("Sending OfferCreate transaction...")
+    // const result = await client.submitAndWait(signed.tx_blob)
+    // if (result.result.meta.TransactionResult == "tesSUCCESS") {
+    //   console.log(`Transaction succeeded:
+    //         https://testnet.xrpl.org/transactions/${signed.hash}`)
+    // } else {
+    //   throw `Error sending transaction: ${result}`
+    // }
   
-    // Check metadata ------------------------------------------------------------
-    // In JavaScript, you can use getBalanceChanges() to help summarize all the
-    const balance_changes = xrpl.getBalanceChanges(result.result.meta)
-    console.log("Total balance changes:", JSON.stringify(balance_changes, null,2))
+    // // Check metadata ------------------------------------------------------------
+    // // In JavaScript, you can use getBalanceChanges() to help summarize all the
+    // const balance_changes = xrpl.getBalanceChanges(result.result.meta)
+    // console.log("Total balance changes:", JSON.stringify(balance_changes, null,2))
   
     // Helper to convert an XRPL amount to a string for display
     function amt_str(amt) {
@@ -160,32 +160,32 @@ exports.createOffer = async (req, res, next) => {
       }
     }
   
-    let offers_affected = 0
-    for (const affnode of result.result.meta.AffectedNodes) {
-      if (affnode.hasOwnProperty("ModifiedNode")) {
-        if (affnode.ModifiedNode.LedgerEntryType == "Offer") {
-          // Usually a ModifiedNode of type Offer indicates a previous Offer that
-          // was partially consumed by this one.
-          offers_affected += 1
-        }
-      } else if (affnode.hasOwnProperty("DeletedNode")) {
-        if (affnode.DeletedNode.LedgerEntryType == "Offer") {
-          // The removed Offer may have been fully consumed, or it may have been
-          // found to be expired or unfunded.
-          offers_affected += 1
-        }
-      } else if (affnode.hasOwnProperty("CreatedNode")) {
-        if (affnode.CreatedNode.LedgerEntryType == "RippleState") {
-          console.log("Created a trust line.")
-        } else if (affnode.CreatedNode.LedgerEntryType == "Offer") {
-          const offer = affnode.CreatedNode.NewFields
-          console.log(`Created an Offer owned by ${offer.Account} with
-            TakerGets=${amt_str(offer.TakerGets)} and
-            TakerPays=${amt_str(offer.TakerPays)}.`)
-        }
-      }
-    }
-    console.log(`Modified or removed ${offers_affected} matching Offer(s)`)
+    // let offers_affected = 0
+    // for (const affnode of result.result.meta.AffectedNodes) {
+    //   if (affnode.hasOwnProperty("ModifiedNode")) {
+    //     if (affnode.ModifiedNode.LedgerEntryType == "Offer") {
+    //       // Usually a ModifiedNode of type Offer indicates a previous Offer that
+    //       // was partially consumed by this one.
+    //       offers_affected += 1
+    //     }
+    //   } else if (affnode.hasOwnProperty("DeletedNode")) {
+    //     if (affnode.DeletedNode.LedgerEntryType == "Offer") {
+    //       // The removed Offer may have been fully consumed, or it may have been
+    //       // found to be expired or unfunded.
+    //       offers_affected += 1
+    //     }
+    //   } else if (affnode.hasOwnProperty("CreatedNode")) {
+    //     if (affnode.CreatedNode.LedgerEntryType == "RippleState") {
+    //       console.log("Created a trust line.")
+    //     } else if (affnode.CreatedNode.LedgerEntryType == "Offer") {
+    //       const offer = affnode.CreatedNode.NewFields
+    //       console.log(`Created an Offer owned by ${offer.Account} with
+    //         TakerGets=${amt_str(offer.TakerGets)} and
+    //         TakerPays=${amt_str(offer.TakerPays)}.`)
+    //     }
+    //   }
+    // }
+    // console.log(`Modified or removed ${offers_affected} matching Offer(s)`)
   
     // Check balances ------------------------------------------------------------
     console.log("Getting address balances as of validated ledger...")
@@ -215,12 +215,12 @@ exports.createOffer = async (req, res, next) => {
                 // "Destination": destination, 
                 // "Amount": amount, 
             },  
-            "user_token": "cf2e0003-eaf7-4bd1-af55-133a6f40db48"  
+            "user_token": "cf2e0003-eaf7-4bd1-af55-133a6f40db48" 
             // "user_token": "2641f74d-c155-4714-91f5-43c5224ffe38"  
           }  
         const subscription = await sdk.payload.createAndSubscribe(request, event => {  
             if(Object.keys(event.data).indexOf('signed') > -1){  
-                return event.data   
+                return event.data  
             }  
         })  
         const resolveData = await subscription.resolved 
@@ -230,7 +230,7 @@ exports.createOffer = async (req, res, next) => {
     console.log(JSON.stringify(acct_offers.result, null, 2))
     res.status(201).json({
       success: true, 
-      data: acct_offers.result,
+      data: acct_offers.result
     });
     client.disconnect()
 }
