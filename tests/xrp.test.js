9bfa00978602f2342fa62372458c5ce4bc5c6679
dorbor <www.dorborrichards.com>
2022-10-03 23:56:37 -0700

    test update

--- tests/xrp.test.js
+++ tests/xrp.test.js
@@ -1,5 +1,31 @@
-describe("XRP Endpoints", () => {
-  it("should return a 201 and send transaction to ledger", async () => {
-    // TODO: Add test body here
-  });
+const  app = require('../app');
+const request = require('supertest');
+
+jest.mock("open")
+jest.mock("xrpl", () => {
+  return {
+    XrpTest: jest.fn().mockImplementation(() => {
+      return {
+        payload: {
+          createAndSubscribe: jest.fn().mockResolvedValueOnce({
+            created: { next: { always: "https://dummy_xrp_url.com" } },
+            resolved : jest.fn().mockResolvedValueOnce({transactionComplete: true, offerAmount: "SomeOfferAmount"})()
+          }),
+          get: jest.fn().mockResolvedValueOnce({application: {OfferCreated: true}}),
+        },
+      };
+    }),
+  };
 });
+
+
+describe("xrp Endpoints", () => {
+    it("should return a 201 and offer amount", async () => {
+      const res = await request(app).get("/createoffer");
+      expect(res.statusCode).toEqual(201);
+      expect(res.body).toHaveProperty("success");
+      expect(res.body).toHaveProperty("offerAmount");
+      expect(res.body.offerAmount).toEqual("dummyofferamount")
+    });
+    // TODO: Add a test for returning a 400 error
+  });
