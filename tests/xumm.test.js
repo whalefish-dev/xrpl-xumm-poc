a6e1b5ca015120df5ece67157a2e3abb979a8d17
amin-nejad <ali@bitfount.com>
2022-10-05 21:52:16 +0100

    Add CI for testing and linting.

--- tests/xumm.test.js
+++ tests/xumm.test.js
@@ -1,7 +1,7 @@
 const request = require("supertest");
 const app = require("../routes/v1/xumm");
 
-jest.mock("open")
+jest.mock("open");
 jest.mock("xumm-sdk", () => {
   return {
     XummSdk: jest.fn().mockImplementation(() => {
@@ -9,9 +9,14 @@ jest.mock("xumm-sdk", () => {
         payload: {
           createAndSubscribe: jest.fn().mockResolvedValueOnce({
             created: { next: { always: "https://dummy_xumm_url.com" } },
-            resolved : jest.fn().mockResolvedValueOnce({signed: true, payload_uuidv4: "some_uuid"})()
+            resolved: jest.fn().mockResolvedValueOnce({
+              signed: true,
+              payload_uuidv4: "some_uuid",
+            })(),
           }),
-          get: jest.fn().mockResolvedValueOnce({application: {issued_user_token: "dummyUserToken"}}),
+          get: jest.fn().mockResolvedValueOnce({
+            application: { issued_user_token: "dummyUserToken" },
+          }),
         },
       };
     }),
@@ -24,7 +29,7 @@ describe("XUMM Endpoints", () => {
     expect(res.statusCode).toEqual(201);
     expect(res.body).toHaveProperty("success");
     expect(res.body).toHaveProperty("userToken");
-    expect(res.body.userToken).toEqual("dummyUserToken")
+    expect(res.body.userToken).toEqual("dummyUserToken");
   });
   // TODO: Add a test for returning a 400 error
 });
